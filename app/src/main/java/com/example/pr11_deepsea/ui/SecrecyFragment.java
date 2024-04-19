ed289598a9fddf7f053ed75b62821719c56eea85
HiteshD <hiteshdeshmukh8998@gmail.com>
2024-04-19 22:39:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pr11_deepsea/ui/SecrecyFragment.java
@@ -0,0 +1,33 @@
+package com.example.pr11_deepsea.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pr11_deepsea.R;
+
+public class SecrecyFragment extends Fragment {
+
+
+
+    public SecrecyFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_secrecy, container, false);
+    }
+}
