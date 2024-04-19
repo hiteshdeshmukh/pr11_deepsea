087e2af360b69944e17b76a5e9f3c5f11b73e934
HiteshD <hiteshdeshmukh8998@gmail.com>
2024-04-19 22:53:00 +0530

    Removed google-services.json from github

--- app/src/main/java/com/example/pr11_deepsea/ui/SecrecyFragment.java
+++ app/src/main/java/com/example/pr11_deepsea/ui/SecrecyFragment.java
@@ -9,14 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pr11_deepsea.R;
+import com.example.pr11_deepsea.databinding.FragmentSecrecyBinding;
 
 public class SecrecyFragment extends Fragment {
 
-
-
-    public SecrecyFragment() {
-        // Required empty public constructor
-    }
+    private FragmentSecrecyBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +25,11 @@ public class SecrecyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_secrecy, container, false);
+        binding = FragmentSecrecyBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+
+
+        return view;
     }
 }
