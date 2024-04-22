99321f854ecabf9839079e0639e75e76cc27267f
HiteshD <hiteshdeshmukh8998@gmail.com>
2024-04-22 22:51:56 +0530

    pending post delete fn noActionBarDone & homeScreen1 Done

--- app/src/main/java/com/example/pr11_deepsea/ui/HomeFragment.java
+++ app/src/main/java/com/example/pr11_deepsea/ui/HomeFragment.java
@@ -21,6 +21,8 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
+        
         return root;
     }
 
