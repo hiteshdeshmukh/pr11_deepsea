13e990baef9ac3b48c43d38bfca79372ebef46c9
HiteshD <hiteshdeshmukh8998@gmail.com>
2024-05-16 19:35:52 +0530

    if condition in test condition and holder - half done background only lightMode homeRecycler done

--- app/src/main/java/com/example/pr11_deepsea/ui/Features/TestQuestionsPage.java
+++ app/src/main/java/com/example/pr11_deepsea/ui/Features/TestQuestionsPage.java
@@ -33,7 +33,49 @@ public class TestQuestionsPage extends AppCompatActivity {
                         getString(R.string.AnxietyQuestion2),getString(R.string.AnxietyQuestion3),
                         getString(R.string.AnxietyQuestion4),getString(R.string.AnxietyQuestion5),
                         getString(R.string.AnxietyQuestion6),getString(R.string.AnxietyQuestion7)));
-            }//else conditions pending
+            } else if (Objects.equals(TitleOfTest, "Depression")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.DepressionTestTitle),getString(R.string.DepressionQuestion1),
+                        getString(R.string.DepressionQuestion2),getString(R.string.DepressionQuestion3),
+                        getString(R.string.DepressionQuestion4),getString(R.string.DepressionQuestion5),
+                        getString(R.string.DepressionQuestion6),getString(R.string.DepressionQuestion7)));
+            } else if (Objects.equals(TitleOfTest, "Personality Disorder")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.PersonalityDisorderTestTitle),getString(R.string.PersonalityQuestion1),
+                        getString(R.string.PersonalityQuestion2),getString(R.string.PersonalityQuestion3),
+                        getString(R.string.PersonalityQuestion4),getString(R.string.PersonalityQuestion5),
+                        getString(R.string.PersonalityQuestion6),getString(R.string.PersonalityQuestion7)));
+            } else if (Objects.equals(TitleOfTest, "Suicidality")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.SucidalityTestTitle),getString(R.string.SucidalityQuestion1),
+                        getString(R.string.SucidalityQuestion2),getString(R.string.SucidalityQuestion3),
+                        getString(R.string.SucidalityQuestion4),getString(R.string.SucidalityQuestion5),
+                        getString(R.string.SucidalityQuestion6),getString(R.string.SucidalityQuestion7)));
+            } else if (Objects.equals(TitleOfTest, "Eating Disorder")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.EatingDisorderTestTitle),getString(R.string.EatingQuestion1),
+                        getString(R.string.EatingQuestion2),getString(R.string.EatingQuestion3),
+                        getString(R.string.EatingQuestion4),getString(R.string.EatingQuestion5),
+                        getString(R.string.EatingQuestion6),getString(R.string.EatingQuestion7)));
+            } else if (Objects.equals(TitleOfTest, "Dissociation")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.DissociationTestTitle),getString(R.string.DissociationQuestion1),
+                        getString(R.string.DissociationQuestion2),getString(R.string.DissociationQuestion3),
+                        getString(R.string.DissociationQuestion4),getString(R.string.DissociationQuestion5),
+                        getString(R.string.DissociationQuestion6),getString(R.string.DissociationQuestion7)));
+            } else if (Objects.equals(TitleOfTest, "ADHD")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.ADHDTestTitle),getString(R.string.ADHDQuestion1),
+                        getString(R.string.ADHDQuestion2),getString(R.string.ADHDQuestion3),
+                        getString(R.string.ADHDQuestion4),getString(R.string.ADHDQuestion5),
+                        getString(R.string.ADHDQuestion6),getString(R.string.ADHDQuestion7)));
+            } else if (Objects.equals(TitleOfTest, "Sleep")) {
+                testQuestionsScreenModelArrayList.add(new TestQuestionsScreenModel(
+                        getString(R.string.SleepTitle),getString(R.string.SleepQuestion1),
+                        getString(R.string.SleepQuestion1),getString(R.string.SleepQuestion1),
+                        getString(R.string.SleepQuestion1),getString(R.string.SleepQuestion1),
+                        getString(R.string.SleepQuestion1),getString(R.string.SleepQuestion1)));
+            }
 
         }
 
